9d40369a287d08b7909fa412d0eefaeece59f3a6
sagiksp <sagi2133.ksp@gmail.com>
2016-05-13 17:30:57 +0300

    Fixed some things with Hebrew text

--- consts.js
+++ consts.js
@@ -125,13 +125,8 @@ var thirteenStrings = [
     "wa’maH wej", // Klingon
     "‘umikūmākolu", // Hawaiian
     "שלוש עשרה", // Hebrew 
-    "שלושעשרה", // Hebrew (without space)
-    "ֹשְלֹש- עֶשְֹרֵה", // Hebrew (with punctuation)
     "שלושה עשר", // Hebrew (male form)
-    "שלושהעשר", // Hebrew (male form, without space)
-    "ֹשְלֹשָה- עָשָֹר", // Hebrew (male form, with punctuation)
     "יג", // Hebrew (gematria)
-    "י״ג", // Hebrew (gematria - apostrophes)
     "quainel", // Quenya
     "mînuiug", // Sindarin
     "tizenhárom", // Hungarian
